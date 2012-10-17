902ef7ff0ad9e58bff84a1be98fe286366865a96
t.ashula <office@ashula.info>
2012-10-17 11:01:02 +0900

    fixes dbxss

--- skin/kanzaki.js
+++ skin/kanzaki.js
@@ -58,11 +58,7 @@ function initvars()
 	gEnv.localhost = gEnv.prv ? "192" : "kanzaki";
 //	gEnv.oAddr = addrelts();
 //	gEnv.stInfo = stinfo();
-	if ( document.location.href.lastIndexOf('?') == -1 ) {
-		gEnv.script = document.location.href;
-	} else {
-		gEnv.script = document.location.href.substring(0,document.location.href.lastIndexOf('?'));
-	}
+        gEnv.script = ((function(l){return l.protocol + '//' + l.host + l.pathname;})(document.location)).replace(/'/g,'&#39;').replace(/"/g,'&quot;');
 	linkattrs(); 
 	window.document.onkeypress = procKey;
 }
